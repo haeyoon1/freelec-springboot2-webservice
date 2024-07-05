7a4fffa6a28d1f26c1b34881f7abb26fe4756ed8
haeyoon1 <hynam1220@naver.com>
2024-07-05 15:31:33 +0900

    initial settings

--- src/main/java/com/jojoldu/book/springboot/Main.java
+++ src/main/java/com/jojoldu/book/springboot/Main.java
@@ -1,4 +1,4 @@
-package com.jojoldu.book;
+package com.jojoldu.book.springboot;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
